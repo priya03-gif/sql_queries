f4a3adcd87aa85e0495885756a1c10f895210244
priya03-gif <117149312+priya03-gif@users.noreply.github.com>
2022-11-01 13:34:18 +0530

    Update ql dql.sql

--- ql dql.sql
+++ ql dql.sql
@@ -9,9 +9,9 @@ CREATE TABLE ZeeRentals(
     Amount BIGINT(20)
 );
 INSERT INTO ZeeRentals VALUES
-('10','CUS01','300','2020-05-21','2022-10-18',80000),
-('20','CUS02','400','2020-05-13','2022-01-20',100000),
-('30','CUS03','350','2020-03-27','2022-12-11',90000);
+('01','CUS1','300','2020-05-21','2022-10-08',80000),
+('02','CUS2','400','2020-05-13','2022-01-22',100000),
+('03','CUS3','350','2020-03-27','2022-12-11',90000);
 SELECT * FROM ZeeRentals;
 
 SELECT Rental_id, Cust_id, Laptop_id, Amount, RPAD(Amount,10,"*") AS Modified_Amount 
